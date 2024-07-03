b54ac34681f421f959feb804721e299939358928
Przybylski Krzysztof <krzysztof.przybylski.ext@siemens.com>
2024-07-03 10:08:21 +0200

    feat: change in lib

--- libs/nx-release-testing/lib2/src/lib/nx-release-testing-lib2.tsx
+++ libs/nx-release-testing/lib2/src/lib/nx-release-testing-lib2.tsx
@@ -3,7 +3,7 @@ import styles from './nx-release-testing-lib2.module.scss';
 export function NxReleaseTestingLib2() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to NxReleaseTestingLib2!</h1>
+      <h1>Welcome to NxReleaseTestingLib2! Some change</h1>
     </div>
   );
 }
